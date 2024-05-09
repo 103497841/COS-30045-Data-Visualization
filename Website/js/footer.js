a93d9cc4ae91f3eb3eecc06cd923f93ae2536eb3
Kylzia <kylziabell@gmail.com>
2024-05-09 15:55:57 +1000

    enhance background, renamed hamburger js to nav js, moved nav in html into nav js, added footer js, moved footer in html into footer js

---
+++ Website/js/footer.js
@@ -0,0 +1,3 @@
+document.write('<footer>');
+    document.write('Footer');
+document.write('</footer>');
---
+++ Website/js/nav.js
@@ -0,0 +1,46 @@
+document.write('<nav>');
+    document.write('<figure class="nav-img">');
+        document.write('<img src="images/swinburne_logo.png" alt="a">');
+    document.write('</figure>');
+
+    document.write('<div class="hamburger">');
+        document.write('<span class="bar"></span>');
+        document.write('<span class="bar"></span>');
+        document.write('<span class="bar"></span>');
+    document.write('</div>');
+
+    document.write('<ul class="nav-main">');
+        document.write('<li>');
+            document.write('<a href="index.html">Home</a>');
+        document.write('</li>');
+        document.write('<li>');
+            document.write('<a href="/" >Test</a>');
+        document.write('</li>');
+        document.write('<li>');
+            document.write('<a href="/" >Test</a>');
+        document.write('</li>');
+        document.write('<li>');
+            document.write('<a href="/" >Test</a>');
+        document.write('</li>');
+        document.write('<li>');
+            document.write('<a href="/" >Test</a>');
+        document.write('</li>');
+    document.write('</ul>');
+document.write('</nav>');
+
+const hamburger = document.querySelector(".hamburger");
+const nav = document.querySelector(".nav-main");
+
+hamburger.addEventListener("click", () => {
+    console.log("hi");
+    hamburger.classList.toggle("active");
+    nav.classList.toggle("active");
+})
+
+const currentPage = document.querySelectorAll(".nav-main li a"); //select all the nav items
+
+currentPage.forEach((page) => {
+    if (page.href === window.location.href) { //if nav item is equal to current page, set aria-current, css will highlight the nav item 
+        page.setAttribute("aria-current", "page");
+    }
+});
