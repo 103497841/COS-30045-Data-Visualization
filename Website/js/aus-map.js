02094e7a427257a242832c55fea9dd10138fed74
Kylzia <kylziabell@gmail.com>
2024-05-17 21:44:55 +1000

    map hover + vic data visual

--- Website/js/aus-map.js
+++ Website/js/aus-map.js
@@ -9,19 +9,29 @@ function init() {
             // loadedSVG = svg.node().append(data.documentElement);
             const loadedSVG = data.documentElement;
 
-            // loadedSVG.setAttribute("viewBox", "0 0 300 300");
-
-            i = 0;
-            const states = ["WA.html","NT.html","NSW.html","ACT.html","VIC.html","TAS.html","QLD.html","SA.html"]
+            i = 0; //cycle through the 'states' array
+            const states = ["WA.html","NT.html","NSW.html","ACT.html","VIC.html","TAS.html","QLD.html","SA.html"] //page links for each states
 
             d3.select(loadedSVG)
                 .selectAll("svg g g")
                 .each(function() {
                     const visual = d3.select(this);
-                    const link = visual.append("a")
+                    const link = visual.append("a") //add anchor to each visual element
                                     .attr("href", states[i]);
 
                     link.append(() => visual.node().cloneNode(true));
+
+                    link.select("g") //hover effects
+                        .on("mouseover",function() {
+                            d3.select(this)
+                                .select("*")
+                                .style("fill","red");
+                        })
+                        .on("mouseout",function() {
+                            d3.select(this)
+                                .select("*")
+                                .style("fill","#b3b3b3");
+                        });
 
                     console.log(i);
                     i++;
