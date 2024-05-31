03cabce861f8120003fe5898396230a4d798c93d
Kylzia <kylziabell@gmail.com>
2024-05-31 17:23:52 +1000

    adjust home page

--- Website/js/footer.js
+++ Website/js/footer.js
@@ -1,3 +1,7 @@
 document.write('<footer>');
-    document.write('Footer');
+    document.write('<p>');
+        document.write('2024 Non-Medical Health Determinants Visualization');
+        document.write('<br>');
+        document.write('Yong Yuan Chong (101224021) & Tsung Sheng Lee (103497841)');
+    document.write('</p>');
 document.write('</footer>');
